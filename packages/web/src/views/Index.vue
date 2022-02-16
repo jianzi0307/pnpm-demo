f3e8364bca9ebe9e8217da5b3538886d36a72bb5
jiangjian <jianzi0307@qq.com>
2022-02-16 09:52:16 +0800

    test

--- packages/web/src/views/Index.vue
+++ packages/web/src/views/Index.vue
@@ -7,26 +7,29 @@
 
 <script>
 export default {
-  name: 'Index',
+  name: "Index",
   mounted() {
     // timerMgr.doOnce()
     this.$timerMgr.doOnce(() => {
-      console.log('timerMgr.doOnce()')
-    }, 1000)
-    
-    this.$storage.setItem('storageKey', 'cookie 过期测试', 5000)
+      console.log("timerMgr.doOnce()");
+    }, 1000);
+
+    // storage
+    this.$storage.setItem("storageKey", "cookie 过期测试", 5000);
     this.$createTimer(
-      'testTimer',
+      "testTimer",
       () => {
-        console.log(this.$storage.getItem('storageKey'))
-        // this.$http
-        //   .get('login')
-        //   .then(res => console.log(res))
-        //   .catch(err => console.log(err))
+        console.log(this.$storage.getItem("storageKey"));
       },
       1000,
       true
-    )
-  }
-}
+    );
+
+    // net
+    this.$http
+      .get("login")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  },
+};
 </script>
