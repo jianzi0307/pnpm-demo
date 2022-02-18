e11ba026a82c0f999311bf53692ad4dcd44a10d9
jiangjian <jianzi0307@qq.com>
2022-02-18 18:04:18 +0800

    fix

--- packages/mobile/postcss.config.js
+++ packages/mobile/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     "postcss-px-to-viewport": {
-      // 设计尺寸 750px = 100vw
+      // 这里配置设计尺寸 750px = 100vw
       viewportWidth: 750,
       unitPrecision: 6,
       unitToConvert: "px",
